adfc20595db4b722f6b1217e1e4d10d2a4a19360
Alex Godwin <alexj.godwin@gmail.com>
2021-07-27 18:51:16 -0400

    got rid of Card

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import faceImage from '../../assets/images/face.jpg';
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -13,7 +12,7 @@ function About() {
         <img src={faceImage} className="my-2 face_img" style={{ width: "30%" }} alt="My face" />       
         </Col>
         <Col>
-        <p className="am_txt">I started to dabble in coding in 2018 with Codecademy. 
+        <p className="d-flex align-items-center am_txt">I started to dabble in coding in 2018 with Codecademy. 
           Some hobbies of mine are: making music, working out, 
           and investing. I like to be to work towards my goals, physically as well 
           as mentally. Thanks for stopping by, and come back soon 
