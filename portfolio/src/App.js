2d7ca4ebbf85dd4f4ce5a56ab5a8c2af3ead0305
Alex Godwin <alexj.godwin@gmail.com>
2021-07-27 15:39:27 -0400

    photo and basic html

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,22 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'portfolio'
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <About></About>
+            <Gallery currentCategory={currentCategory}></Gallery>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+
+      <footer>
+        <div className="icons">
+          <a href="https://github.com/AlexJG501" targer="_blank" rel="noreferrer" title="GitHub">
+            GitHub
+          </a>
+          <a href="#" targer="_blank" rel="noreferrer" title="LinkedIn">
+            LinkedIn
+          </a>
+          <a href="https://stackoverflow.com/users/16479419/ronan-codes" targer="_blank" rel="noreferrer" title="GitHub">
+            Stack
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
